32b43cb21bf586146975d5fc2590893db7063dfa
Daniel Dvorkin <102325794+ddvorkin14@users.noreply.github.com>
2023-01-23 12:31:52 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const userController = require("../controllers/users");
-
+router.get("/users", userController.getUsers);
 router.post("/create-user", userController.createUser);
 
 module.exports = router;
